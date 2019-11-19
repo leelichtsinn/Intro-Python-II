02084b1baf3e1460cc6f42f1c47a314954b842ec
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-11-19 10:26:44 -0500

    added Player class defined init and str methods

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+class Player(self, name, current_room):
+    """docstring for Player."""
+
+    def __init__(self):
+        self.name = name
+        self.current_room = current_room
+
+    def __str__(self):
+        output = f'Player name: {self.name} \n'
+        output += f'Location: {self.current_room}'
+        return output
