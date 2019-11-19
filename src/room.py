e301b0fcbfb182ede9cf20611cef29b4163a79ff
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-11-19 12:50:29 -0500

    added Room class added init and str methods

--- src/room.py
+++ src/room.py
@@ -1,2 +1,17 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+class Room(self, name, description, items):
+    """docstring for Room."""
+
+    def __init__(self):
+        self.name = name
+        self.description = description
+        self.n_to = None
+        self.s_to = None
+        self.e_to = None
+        self.w_to = None
+
+    def __str__(self):
+        output = f'Room: {self.name} \n'
+        output += f'Despcription: {self.description}'
+        return output
