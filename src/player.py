159f90b23d2b9248ab3310684b5b9fc46d7d090e
Lee Lichtsinn <lee.lichtsinn@gmail.com>
2019-11-19 23:36:06 -0500

    minor refactoring and code cleanup

--- src/player.py
+++ src/player.py
@@ -1,11 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
-class Player(self, name, current_room):
-    """docstring for Player."""
-
-    def __init__(self):
+class Player:
+    def __init__(self, name, current_room):
         self.name = name
-        self.current_room = current_room
+        self.room = current_room
 
     def __str__(self):
         output = f'Player name: {self.name} \n'
